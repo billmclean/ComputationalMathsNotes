fa705325f7addc10b76e1b0d52f71c76f9a4d41a
William McLean <w.mclean@unsw.edu.au>
2018-09-21 16:59:22 +1000

    python scripts for figures in chapter 6

---
+++ src/chap6/bad_triangulation.py
@@ -0,0 +1,41 @@
+from matplotlib.tri import Triangulation
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([-2,  2, 2, 1, -1, -2, 0,  0])
+y = np.array([-2, -2, 0, 2,  2,  0, 0, -2])
+triangles = np.array([[0, 7, 5],
+                      [2, 5, 7],
+                      [1, 2, 7],
+                      [2, 3, 6],
+                      [3, 4, 6],
+                      [4, 5, 6]])
+mypoly = Triangulation(x, y, triangles)
+n_pts = len(x)
+n_tri = triangles.shape[0]
+
+def centroid(j, tri):
+    x = tri.x[tri.triangles[j,:]]
+    y = tri.y[tri.triangles[j,:]]
+    return sum(x)/3, sum(y)/3
+
+plt.triplot(mypoly, color='k')
+plt.plot(x, y, 'or')
+x_offset = 0.1 * np.array([-1,  1, 1, 1, -1, -1,  0,  0])
+y_offset = 0.1 * np.array([-1, -1, 0, 1,  1,  0, -2, -2])
+
+for j in range(n_pts):
+    plt.text(x[j]+x_offset[j], y[j]+y_offset[j], j+1, color='r',
+             verticalalignment='center', horizontalalignment='center')
+
+for j in range(n_tri):
+    xc, yc = centroid(j, mypoly)
+    plt.text(xc, yc, j+1, color='b',
+             verticalalignment='center', horizontalalignment='center')
+  
+
+ax = plt.gca()
+ax.axis([-2.2, 2.2, -2.2, 2.2])
+ax.axis('off')
+plt.show()
+plt.savefig('bad_triangulation.pdf')
---
+++ src/chap6/tent_func.py
@@ -0,0 +1,25 @@
+from matplotlib.tri import Triangulation
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+x = np.array([-1, 0, 1])
+y = np.array([-1, 0, 1])
+X, Y = np.meshgrid(x, y)
+x, y = X.flatten(), Y.flatten()
+mygrid = Triangulation(x, y)
+
+Z = np.zeros_like(X)
+Z[1,1] = 1.0
+z = Z.flatten()
+
+fig = plt.figure(1)
+ax = fig.add_subplot(111, projection='3d')
+ax.triplot(mygrid, color='k')
+ax.plot_trisurf(mygrid, z, color='c', alpha=0.5)
+plt.axis(azimuth=-70, elevation=30)
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
+plt.savefig('tent_func.pdf')
