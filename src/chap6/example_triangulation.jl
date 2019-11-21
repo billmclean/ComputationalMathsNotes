485eeda8db68a99b663e401c8e12a3dc89bc32af
William McLean <w.mclean@unsw.edu.au>
2019-11-22 10:54:35 +1100

    triangulation examples

---
+++ src/chap6/example_triangulation.jl
@@ -0,0 +1,25 @@
+using PyPlot
+using Triangulations
+
+N = [ -1  -1  -1  0   0  1  0
+       2   0  -2  0  -2  0  2 ]
+
+T = [ 6  7  1  2  3  4
+      7  2  2  3  5  5
+      4  4  7  4  4  6 ]
+
+E = [ 7  1  2  3  5  6
+      1  2  3  5  6  7 ]
+
+QN = 4
+
+tri = Triangulation(N, T, E, 4)
+
+figure(1)
+draw_triangles(tri)
+enumerate_triangles(tri)
+enumerate_vertices(tri)
+enumerate_bdry_edges(tri, 0.1)
+mark_first_local_vertex(tri, 0.3)
+axis("off")
+savefig("example_triangulation.pdf")
---
+++ src/chap6/exercise_triangulation.jl
@@ -0,0 +1,46 @@
+using PyPlot
+using Triangulations
+
+N = Float64[ 1  0  1  0  -1   0  -1
+             1  1  0  0   0  -1  -1 ]
+
+T = [ 1 4 4 3 4 5
+      2 3 2 4 5 7
+      3 2 5 6 6 6 ]
+
+E = [ 6 3 1 2 5 7
+      3 1 2 5 7 6 ]
+
+tri = Triangulation(N, T, E, 4)
+
+figure(1)
+draw_triangles(tri)
+enumerate_triangles(tri)
+enumerate_vertices(tri)
+mark_first_local_vertex(tri, 0.15)
+enumerate_bdry_edges(tri, 0.1)
+axis("off")
+axis("equal")
+savefig("ex1_triangulation.pdf")
+
+N = Float64[ 4  2  0  1  3  5  2  4  6  6
+             2  2  2  1  1  1  0  0  0  2 ]
+
+T = [ 3  5  4  7  6  1  5   6  10  8
+      4  2  7  8  5  2  6   9   1  9
+      2  4  5  5  8  5  1  10   6  6 ]
+
+E = [ 10  1  2  3  4  7  8   9
+       1  2  3  4  7  8  9  10 ]
+
+tri = Triangulation(N, T, E, 5)
+
+figure(2)
+draw_triangles(tri)
+enumerate_triangles(tri)
+enumerate_vertices(tri)
+mark_first_local_vertex(tri, 0.3)
+enumerate_bdry_edges(tri, 0.2)
+axis("off")
+axis("equal")
+savefig("ex2_triangulation.pdf")
